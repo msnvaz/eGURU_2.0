59e969d728feb0025c9ecb84a7b6f63ed7a1b0a6
Sandeep Vaz <vazsandeep@gmail.com>
2025-02-11 23:37:23 +0530

    AdminTransactions

---
+++ src/Views/admin/AdminTransactions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eGURU Admin</title>
+    <link rel="icon" type="image/png" href="/images/eGURU_6.png">
+    <link rel="stylesheet" href="/css/admin/Admin.css">
+    <link rel="stylesheet" href="/css/admin/AdminHeader.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'AdminHeader.php'; ?>
+    <?php include 'AdminNav.php'; ?>
+    
+    <div class="main">
+    <table>
+</table>
+</body>
+</html>
