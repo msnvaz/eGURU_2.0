66b7b702ec0d1104f7c64277ed90b97f31fad6dc
thiruveni2002 <thiruveni976@gmail.com>
2025-04-26 11:13:56 +0530

    student inbox changes

--- src/Controllers/student/StudentSignupController.php
+++ src/Controllers/student/StudentSignupController.php
@@ -29,6 +29,12 @@ class StudentSignupController {
             $student_DOB = $_POST['date'];  // Changed from student_DOB to date
             $student_phonenumber = $_POST['tel'];  // Changed from student_phonenumber to tel
     
+            if (!preg_match('/^(?=.*[a-z])(?=.*[@$!%*?&])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/', $student_password)) {
+                $_SESSION['signup_error'] = "Password must be strong.";
+                header("Location: /student-signup");
+                exit();
+            }
+
             // Validate date of birth (must be above 10 years old)
             $dob = new \DateTime($student_DOB);
             $currentDate = new \DateTime();
